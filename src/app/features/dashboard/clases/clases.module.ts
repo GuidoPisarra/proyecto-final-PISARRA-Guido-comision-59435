f3dce375fd960bd03d121bc75d059b65726f8ca4
pisarraguido <pisarraguido@gmail.com>
2024-10-24 01:18:18 -0300

    ajustes routing clases y courses

--- src/app/features/dashboard/clases/clases.module.ts
+++ src/app/features/dashboard/clases/clases.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClasesComponent } from './clases.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { ClasesRoutingModule } from './clases-routing.module';
 
 @NgModule({
   declarations: [ClasesComponent],
   imports: [
     CommonModule,
-    SharedModule],
+    SharedModule,
+    ClasesRoutingModule
+  ],
   exports: [ClasesComponent]
 })
 export class ClasesModule { }
--- src/app/features/dashboard/courses/courses.module.ts
+++ src/app/features/dashboard/courses/courses.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { CoursesRoutingModule } from './courses-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { SharedModule } from '../../../shared/shared.module';
   declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    CoursesRoutingModule
   ],
   exports: [CoursesComponent]
 })
